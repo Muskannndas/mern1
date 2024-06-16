11c1131a9f949f11d27cbf0637a3153b4954e6f4
Muskannndas <135194758+Muskannndas@users.noreply.github.com>
2024-06-16 18:00:53 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const MainContent = () => {
+  return (
+    <div id="page-content-wrapper">
+      <div className="container-fluid">
+        <h1 className="mt-4">Main Content</h1>
+        <p>This is the main content area.</p>
+      </div>
+    </div>
+  );
+}
+
+export default MainContent;
